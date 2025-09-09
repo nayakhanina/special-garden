34b055327ec7a8764be364d5dfa15850e0fb7bf3
nayakhanina <nayakhanina@gmail.com>
2025-09-09 16:30:56 +0300

    остальные суммы

--- main.js
+++ main.js
@@ -35,8 +35,19 @@ function incrementTomato() {
 }
 
 function pricecarrot() {
-    let carrotinput = 
-    document.querySelector("[name='carrotinput']").value;
-    sum = pricecarrot * carrot
+    let carrotinput = document.querySelector("[name='carrotinput']").value;
+    sum = carrotinput * carrot
     document.querySelector('#pricecarrotsum').innerHTML = sum
+}
+
+function priceonion() {
+    let onioninput = document.querySelector("[name='onioninput']").value;
+    sum = onioninput * onion
+    document.querySelector('#priceonionsum').innerHTML = sum
+}
+
+function pricetomato() {
+    let tomatoinput = document.querySelector("[name='tomatoinput']").value;
+    sum = tomatoinput * tomato
+    document.querySelector('#pricetomatosum').innerHTML = sum
 }
